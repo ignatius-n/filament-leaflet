7d08f7ac256e7df06d55c8c6f88b467faca1c364
Julian Lugod <6212018+akiraaisha@users.noreply.github.com>
2026-02-13 00:40:13 +0800

    Update TileLayer.php
    Added Mapbox tile.

--- src/Enums/TileLayer.php
+++ src/Enums/TileLayer.php
@@ -9,7 +9,7 @@ enum TileLayer: string implements HasLabel
 {
     // OpenStreetMap
     case OpenStreetMap = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    
+
     // Google Maps
     case GoogleStreets = 'http://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}';
     case GoogleSatellite = 'http://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}';
@@ -25,6 +25,13 @@ enum TileLayer: string implements HasLabel
     case CartoPositron = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png';
     case CartoDarkMatter = 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png';
 
+    // Mapbox (placeholder values - use getUrl() method for proper URLs with env variables)
+    case MapboxStreets = 'mapbox://styles/mapbox/streets-v11';
+    case MapboxDark = 'mapbox://styles/mapbox/dark-v11';
+    case MapboxSatellite = 'mapbox://styles/mapbox/satellite-v9';
+    case MapboxNavigationDay = 'mapbox://styles/mapbox/navigation-day-v1';
+    case MapboxNavigationNight = 'mapbox://styles/mapbox/navigation-night-v1';
+
     public function getLabel(): ?string
     {
         $name = preg_replace('/(?<!^)[A-Z]/', ' $0', $this->name);
@@ -35,18 +42,27 @@ enum TileLayer: string implements HasLabel
     {
         return match ($this) {
             self::OpenStreetMap => '&copy; OpenStreetMap contributors',
-            
-            self::GoogleStreets, 
-            self::GoogleSatellite, 
-            self::GoogleHybrid, 
+
+            self::GoogleStreets,
+            self::GoogleSatellite,
+            self::GoogleHybrid,
             self::GoogleTerrain => '&copy; Google Maps',
-            
-            self::EsriWorldImagery, 
-            self::EsriWorldStreetMap, 
+
+            self::EsriWorldImagery,
+            self::EsriWorldStreetMap,
             self::EsriNatGeo => 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-            
-            self::CartoPositron, 
+
+            self::CartoPositron,
             self::CartoDarkMatter => '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+
+            self::MapboxStreets,
+            self::MapboxDark,
+            self::MapboxNavigationDay,
+            self::MapboxNavigationNight,
+            self::MapboxSatellite => '&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//            self::MapboxNavigationDay,
+//            self::MapboxNavigationNight,
+
         };
     }
 
@@ -75,5 +91,41 @@ enum TileLayer: string implements HasLabel
             self::CartoPositron,
             self::CartoDarkMatter,
         ];
+    }
+
+    public static function mapboxLayers(): array
+    {
+        return [
+            self::MapboxStreets,
+            self::MapboxDark,
+            self::MapboxSatellite,
+            self::MapboxNavigationDay,
+            self::MapboxNavigationNight,
+        ];
+    }
+
+    public function getUrl(): string
+    {
+        if (in_array($this, self::mapboxLayers(), true)) {
+            $accessToken = config('services.mapbox.token');
+            $tileSize = config('services.mapbox.tile_size', 512);
+
+            if (!$accessToken) {
+                throw new \InvalidArgumentException('MAPBOX_ACCESS_TOKEN environment variable is required for Mapbox tile layers.');
+            }
+
+            $styleId = match ($this) {
+                self::MapboxStreets => 'streets-v11',
+                self::MapboxDark => 'dark-v11',
+                self::MapboxSatellite => 'satellite-v9',
+                self::MapboxNavigationDay => 'navigation-day-v1',
+                self::MapboxNavigationNight => 'navigation-night-v1',
+                default => 'streets-v11',
+            };
+
+            return "https://api.mapbox.com/styles/v1/mapbox/{$styleId}/tiles/{$tileSize}/{z}/{x}/{y}?access_token={$accessToken}";
+        }
+
+        return $this->value;
     }
 }
